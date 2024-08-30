3155f0a9d602c24a1ed9ae859cdba8726918bf7c
Anuj Subedi <anujsubedi@gmail.com>
2024-08-30 15:20:22 +0545

    replaced to with and

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Welcome to My React App</h1>
-        <p>This is a simple React app deployed using Jenkins to AWS S3.</p>
+        <p>This is a simple React app deployed using Jenkins and AWS S3.</p>
       </header>
     </div>
   );
